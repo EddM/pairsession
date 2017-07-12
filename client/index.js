c9799c507145e2684ab954877a9c0f8a44fa8bf1
Edd Morgan <eddm@me.com>
2017-07-12 22:43:44 +0100

    actioncable support

--- client/index.js
+++ client/index.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+import ActionCable from 'actioncable';
+
+const cable = ActionCable.createConsumer('wss://localhost:3000/cable')
+
+cable.subscriptions.create({
+  channel: 'DocumentChannel',
+  name: 'foo'
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
