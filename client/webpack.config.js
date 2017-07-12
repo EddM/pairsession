0a5f376eb9f65e44e867778aeca105ab17843453
Edd Morgan <eddm@me.com>
2017-07-12 22:37:20 +0100

    stylesheets

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,24 +1,22 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: 'index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './index.js',
+  entry: ['./index.js', './styles/index.scss'],
   output: {
     path: path.resolve('dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css-loader!sass-loader') }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html', filename: 'index.html', inject: 'body' }),
+    new ExtractTextPlugin('./styles/index.css', { allChunks: true }),
+  ]
 };
